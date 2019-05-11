738e2805e54e91f3199fb2f69d035334e1707984
odan <odan3240@gmail.com>
2019-05-12 00:30:52 +0900

    いい感じにした

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,7 +1,8 @@
 import "reflect-metadata"; // this shim is required
-import { createExpressServer } from "routing-controllers";
+import { useExpressServer } from "routing-controllers";
 import consola from "consola";
 import morgan from "morgan";
+import express from "express";
 const { Nuxt, Builder } = require("nuxt");
 
 import { UserController } from "./UserController";
@@ -10,6 +11,7 @@ async function start() {
   let config = require("../../frontend/nuxt.config.js");
   config.dev = !(process.env.NODE_ENV === "production");
   config.srcDir = "../frontend/";
+  config.buildDir = "../frontend/.nuxt";
   const nuxt = new Nuxt(config);
 
   if (config.dev) {
@@ -19,14 +21,23 @@ async function start() {
     await nuxt.ready();
   }
 
-  // creates express app, registers all controller routes and returns you express app instance
-  const app = createExpressServer({
-    controllers: [UserController] // we specify controllers we want to use
-  });
+  const app = express();
 
   app.use(morgan("combined"));
+  app.use((req, res, next) => {
+    if (req.path.startsWith("/api")) {
+      next();
+    } else {
+      nuxt.render(req, res, next);
+    }
+  });
 
-  app.use(nuxt.render);
+  useExpressServer(app, {
+    controllers: [UserController],
+    routePrefix: "api"
+  });
+
+  // app.use(/^(?!\/api).+$/, nuxt.render);
 
   consola.info(`start listen at ${3000}`);
   // run express application on port 3000
---
+++ frontend/pages/poyo.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    poyo
+  </div>
+</template>
